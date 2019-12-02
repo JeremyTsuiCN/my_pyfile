8df2900aaca8a6a300d3c10148df118a7717f1d4
JeremyTsuiCN <46662768+JeremyTsuiCN@users.noreply.github.com>
2019-12-02 18:07:52 +0800

    Update compare_stu(0.1).py
    新增了一些中文备注

--- compare_stu(0.1).py
+++ compare_stu(0.1).py
@@ -5,20 +5,20 @@ class CreateList():
     n = 0
     def __init__(self,date):
         self.date = date
-        self.n = 0
         
     
-    def read_stu_data(self):
-        CreateList.n+=1
+    def read_stu_data(self):  #用于读取两个csv的数据，并把结果封装在两个字典里
+        CreateList.n+=1     #通过n的值判断放进哪个字典中
         old_data = {}
         new_data = {}
+        
         with open('./original_data/{}.csv'.format(self.date),encoding = 'utf-8',errors="ignore") as file:
             reader = csv.reader(file)
-            
             for data in reader:
-                
-                wx_name = re.findall(r'(.*?)\nwxid',data[0])
-                level = data[1]
+                wx_name = re.findall(r'(.*?)\nwxid',data[0])  #获取微信名
+                level = data[1] #获取当前关卡
+                #此处之后需要再添加一个备注功能，用于判断是否在加班，生病，
+                #或备注第几关奖励已经发放情况
                 try:
                     if CreateList.n == 1:
                         old_data[wx_name[0]]=level
@@ -29,7 +29,7 @@ class CreateList():
             return old_data,new_data
 
         
-def parse(*args):
+def parse(*args):    #两个文件名作为参数传入，并解析
     old_data = CreateList(args[0]).read_stu_data()[0]
     new_data = CreateList(args[1]).read_stu_data()[1]
     lazy_stu = []
@@ -53,20 +53,23 @@ def parse(*args):
     return lazy_stu,good_stu,new_stu
 
 
-old_date = input('please input a date before today: ')
+old_date = input('please input the old document'\s name: \n')
 today =  input('what\'s the date today??')
-my_parse = parse(old_date,today)       
+my_parse = parse(old_date,today)       #调用这个函数
 
-def outPut(all_list):
+def moveSit(stu_list):    #学习动态情况分析，分三个类别。有进度的，无进度的，新进班的。
     with open('output.csv','w') as file:
         writer = csv.writer(file)
         writer.writerow(['学员情况说明'])
-        all_list[0].insert(0,'没有学习：')
-        all_list[1].insert(0,'勤奋学习：')
-        all_list[2].insert(0,'新增学员：')
-        writer.writerow(all_list[0])
-        writer.writerow(all_list[1])
-        writer.writerow(all_list[2])
+        stu_list[0].insert(0,'没有学习：')
+        stu_list[1].insert(0,'勤奋学习：')
+        stu_list[2].insert(0,'新增学员：')
+        writer.writerow(stu_list[0])
+        writer.writerow(stu_list[1])
+        writer.writerow(stu_list[2])
 
-outPut(my_parse)
+def levelParse():
+    pass
+                 
+             
 
